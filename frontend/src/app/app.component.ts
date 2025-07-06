f9562f91b236e7c43f82151e4f26a6313c295e4f
abeatrizreisdev <abeatrizreis1@gmail.com>
2025-07-06 18:27:03 -0300

    banner-carrosel complet

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BannerCarrosselComponent } from './banner-carrossel/banner-carrossel.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, BannerCarrosselComponent],
   template: `
   <app-header></app-header>
   <main>
     <router-outlet></router-outlet>
+    <app-banner-carrossel></app-banner-carrossel>
   </main>
   <app-footer></app-footer>
 `,
---
+++ frontend/src/app/banner-carrossel/banner-carrossel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-carrossel',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner-carrossel.component.html',
+  styleUrl: './banner-carrossel.component.css'
+})
+export class BannerCarrosselComponent {
+
+}
